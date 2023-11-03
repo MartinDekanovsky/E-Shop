a6b0c0ba327cdc1663f4d08bfde9aa9a9f62d228
Martin Dekanovský <martin.dekanovsky94@gmail.com>
2023-11-03 09:51:23 +0100

    Frontend and backend modules created.

---
+++ NonekuptoWebParent/NonekuptoBackEnd/src/main/java/sk/nonekupto/admin/MainController.java
@@ -0,0 +1,13 @@
+package sk.nonekupto.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+}
---
+++ NonekuptoWebParent/NonekuptoBackEnd/src/main/java/sk/nonekupto/admin/NonekuptoBackEndApplication.java
@@ -0,0 +1,13 @@
+package sk.nonekupto.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonekuptoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonekuptoBackEndApplication.class, args);
+	}
+
+}
